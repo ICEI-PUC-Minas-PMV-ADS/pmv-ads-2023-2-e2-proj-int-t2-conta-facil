db043fff21556c005029aa6fe3d270d8c3872cbc
DiogoSilva07 <diogovictor190@gmail.com>
2023-12-04 22:36:58 -0300

    Atualização Login e Banco de Dados

---
+++ src/ContaFacil2/dbo.Cadastros.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Cadastros] (
+    [Email]     NVARCHAR (450) NOT NULL,
+    [Nome]      NVARCHAR (MAX) NULL,
+    [Logadouro] NVARCHAR (MAX) NOT NULL,
+    [Cidade]    NVARCHAR (MAX) NOT NULL,
+    [Uf]        NVARCHAR (MAX) NOT NULL,
+    [CEP]       NVARCHAR (MAX) NOT NULL,
+    [Telefone]  NVARCHAR (MAX) NOT NULL,
+    [Cpf]       NVARCHAR (MAX) NOT NULL,
+    [Senha]     NVARCHAR (MAX) NOT NULL,
+    [Perfil]    NVARCHAR (MAX) NOT NULL,
+    CONSTRAINT [PK_Cadastros] PRIMARY KEY CLUSTERED ([Email] ASC)
+);
+
